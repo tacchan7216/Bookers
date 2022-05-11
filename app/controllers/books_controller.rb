e3945f63f359f38241c435a88334560de6ba6ec1
tacchan7216 <104713690+tacchan7216@users.noreply.github.com>
2022-05-11 02:19:18 +0000

    [update]destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,12 +18,27 @@ class BooksController < ApplicationController
   end
 
   def show
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  
+  
 
   private
   #ストロングパラメータ
