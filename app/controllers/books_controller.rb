6339667783ca3c37ba53b778cb17ff32e106aadc
tacchan7216 <104713690+tacchan7216@users.noreply.github.com>
2022-05-11 13:44:55 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,26 @@
 class BooksController < ApplicationController
   def new
-   @book = Book.new
+
   end
 
   def create
+
     #データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     #データをデータベースに保存するためのsaveメソッド実行
-      if book.save
+    if @book.save
     #詳細画面へリダイレクトし、フラッシュメッセージを表示
-    redirect_to book_path(book.id), notice: 'Book was successfully updated'
-      else
-        render new_book
-      end  
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+        @books = Book.all
+        render :index
+    end
   end
 
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -27,26 +30,30 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+    render :edit
+    end
+
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      redirect_to books_path, notice: 'Book was successfully destroyed.'
+    end
   end
 
-  
-  
+
+
 
   private
   #ストロングパラメータ
     def book_params
       params.require(:book).permit(:title,:body)
     end
-
 end
